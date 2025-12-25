fc0a415a2dd41e2ee5f1770a647ed837150a5b24
pKondaszewski <kondaszewskiprz@gmail.com>
2025-12-26 00:40:08 +0100

    Init commit

---
+++ src/main/java/pl/przemek/storage_buddy/StorageBuddyApplication.java
@@ -0,0 +1,13 @@
+package pl.przemek.storage_buddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorageBuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StorageBuddyApplication.class, args);
+	}
+
+}
