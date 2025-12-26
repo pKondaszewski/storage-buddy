067f70cbc951d8a621a594f21f4206bb2254daff
pKondaszewski <kondaszewskiprz@gmail.com>
2025-12-26 17:34:22 +0100

    refactor(file): add defined ingore mapping on id and createdAt

--- src/main/java/pl/przemek/storage_buddy/file/FileMapper.java
+++ src/main/java/pl/przemek/storage_buddy/file/FileMapper.java
@@ -1,11 +1,14 @@
 package pl.przemek.storage_buddy.file;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import pl.przemek.storage_buddy.file.dto.CreateFileRequest;
 import pl.przemek.storage_buddy.file.dto.FileResponse;
 
 @Mapper
 interface FileMapper {
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
     File toEntity(CreateFileRequest request);
 
     FileResponse toResponse(File entity);
