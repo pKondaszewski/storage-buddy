2f53b5b352d6018d76c37455581a6ca399419e14
pKondaszewski <kondaszewskiprz@gmail.com>
2025-12-26 17:27:05 +0100

    fix(test): add missing testcontainers config

--- src/test/java/pl/przemek/storage_buddy/StorageBuddyApplicationTests.java
+++ src/test/java/pl/przemek/storage_buddy/StorageBuddyApplicationTests.java
@@ -2,8 +2,10 @@ package pl.przemek.storage_buddy;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 @SpringBootTest
+@Import(TestcontainersConfig.class)
 class StorageBuddyApplicationTests {
 
     @Test
