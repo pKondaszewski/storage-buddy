cf9769052ab189107a9d12d6b435ad1a7fdb9eed
pKondaszewski <kondaszewskiprz@gmail.com>
2025-12-27 22:56:11 +0100

    test(mapper): add more mapper tests

--- src/test/java/pl/przemek/storage_buddy/file/FileMapperTest.java
+++ src/test/java/pl/przemek/storage_buddy/file/FileMapperTest.java
@@ -4,13 +4,16 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
+import java.util.UUID;
 import org.junit.jupiter.api.Test;
 import pl.przemek.storage_buddy.file.dto.CreateFileInfoRequest;
+import pl.przemek.storage_buddy.file.dto.CreatedFileInfoResponse;
 
 class FileMapperTest {
 
     private static final String FILENAME = "name.txt";
     private static final CreateFileInfoRequest CREATE_FILE_REQUEST = new CreateFileInfoRequest(FILENAME);
+    private static final FileInfo FILE_INFO = new FileInfo();
 
     private final FileMapper mapper = new FileMapperImpl();
 
@@ -39,5 +42,36 @@ class FileMapperTest {
 
         // then
         assertEquals(FILENAME, result.getName());
+    }
+
+    @Test
+    void shouldCorrectlyMapNullToEntity() {
+        // when
+        FileInfo result = mapper.toEntity(null);
+
+        // then
+        assertNull(result);
+    }
+
+    @Test
+    void shouldCorrectlyMapNullToResponse() {
+        // when
+        CreatedFileInfoResponse result = mapper.toResponse(null);
+
+        // then
+        assertNull(result);
+    }
+
+    @Test
+    void shouldCorrectlyMapIdDuringMappingToResponse() {
+        // given
+        UUID expected = UUID.randomUUID();
+        FILE_INFO.setId(expected);
+
+        // when
+        CreatedFileInfoResponse result = mapper.toResponse(FILE_INFO);
+
+        // then
+        assertEquals(expected, result.id());
     }
 }
