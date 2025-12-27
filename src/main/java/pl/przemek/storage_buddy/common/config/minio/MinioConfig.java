0bb46e494421c28418e747f2fe0780773014c786
pKondaszewski <kondaszewskiprz@gmail.com>
2025-12-27 21:14:58 +0100

    refactor(minio): provide minio setup

---
+++ src/main/java/pl/przemek/storage_buddy/common/config/minio/MinioConfig.java
@@ -0,0 +1,23 @@
+package pl.przemek.storage_buddy.common.config.minio;
+
+import io.minio.MinioClient;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties(MinioProperties.class)
+@RequiredArgsConstructor
+class MinioConfig {
+
+    private final MinioProperties minioProperties;
+
+    @Bean
+    MinioClient minioClient() {
+        return MinioClient.builder()
+                .endpoint(minioProperties.endpoint())
+                .credentials(minioProperties.accessKey(), minioProperties.secretKey())
+                .build();
+    }
+}
