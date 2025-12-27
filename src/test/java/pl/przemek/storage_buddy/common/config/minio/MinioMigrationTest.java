23c5967a3b7a32253940a0e36a7b6ee3f2dc2389
pKondaszewski <kondaszewskiprz@gmail.com>
2025-12-27 22:05:09 +0100

    feat(minio): minio migration tests

---
+++ src/test/java/pl/przemek/storage_buddy/common/config/minio/MinioMigrationTest.java
@@ -0,0 +1,61 @@
+package pl.przemek.storage_buddy.common.config.minio;
+
+import io.minio.BucketExistsArgs;
+import io.minio.MakeBucketArgs;
+import io.minio.MinioClient;
+import io.minio.errors.InsufficientDataException;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+class MinioMigrationTest {
+
+    private static final String BUCKET_NAME = "test-bucket";
+    private static final BucketExistsArgs EXISTS_BUCKET_ARGS =
+            BucketExistsArgs.builder().bucket(BUCKET_NAME).build();
+    private static final MakeBucketArgs MAKE_BUCKET_ARGS =
+            MakeBucketArgs.builder().bucket(BUCKET_NAME).build();
+
+    private final MinioClient minioClient = mock(MinioClient.class);
+    private final MinioProperties minioProperties = new MinioProperties(
+            "endpoint", "accessKey", "secretKey", new MinioProperties.BucketProperties(BUCKET_NAME));
+    private final MinioMigration minioMigration = new MinioMigration(minioClient, minioProperties);
+
+    @Test
+    void shouldCreateNonExistingBucket() throws Exception {
+        // given
+        when(minioClient.bucketExists(EXISTS_BUCKET_ARGS)).thenReturn(false);
+
+        // when
+        minioMigration.migrate();
+
+        // then
+        verify(minioClient, times(1)).makeBucket(MAKE_BUCKET_ARGS);
+    }
+
+    @Test
+    void shouldNotCreateAlreadyExistingBucket() throws Exception {
+        // given
+        when(minioClient.bucketExists(EXISTS_BUCKET_ARGS)).thenReturn(true);
+
+        // when
+        minioMigration.migrate();
+
+        // then
+        verify(minioClient, times(0)).makeBucket(MAKE_BUCKET_ARGS);
+    }
+
+    @Test
+    void throwExceptionWhenMinioClientReturnsException() throws Exception {
+        // given
+        when(minioClient.bucketExists(EXISTS_BUCKET_ARGS))
+                .thenThrow(new InsufficientDataException(null));
+
+        // when then
+        assertThrows(InsufficientDataException.class, minioMigration::migrate);
+    }
+}
